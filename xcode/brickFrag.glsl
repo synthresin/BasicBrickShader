66eef7efca4b3b36abf954f8d7ec237c6eaa0c03
synthresin <synthresin@me.com>
2012-11-15 13:09:58 +0900

    asd

--- xcode/brickFrag.glsl
+++ xcode/brickFrag.glsl
@@ -8,7 +8,7 @@ void main() {
     vec3 color;
 
     
-    color = vec3(0.6,0.9,0.3);
+    color = texture2D(tex, TexCoord).rgb;
     color *= LightIntensity;
     gl_FragColor = vec4(color,1.0);
     
