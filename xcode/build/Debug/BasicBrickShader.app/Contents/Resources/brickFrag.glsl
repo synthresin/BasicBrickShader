a7c19f941b6a52e62e8e68c24e7e5ec8c8105fc0
synthresin <synthresin@me.com>
2012-11-16 12:14:20 +0900

    marble complete

--- xcode/build/Debug/BasicBrickShader.app/Contents/Resources/brickFrag.glsl
+++ xcode/build/Debug/BasicBrickShader.app/Contents/Resources/brickFrag.glsl
@@ -13,8 +13,8 @@ void main() {
     
     float intensity = abs(noisevec[0] - 0.25) + abs(noisevec[1] - 0.125) + abs(noisevec[2] - 0.0625) + abs(noisevec[3]-0.03125);
     
-    intensity = clamp(intensity * 6.0, 0.0, 1.0);
+    float sineval = sin(MCPosition.y * 6.0 + intensity * 12.0) * 0.5 + 0.5;
     
-    vec3 color = mix(SkyColor, CloudColor, intensity) * LightIntensity;
+    vec3 color = mix(SkyColor, CloudColor, sineval) * LightIntensity;
     gl_FragColor = vec4(color, 1.0);
 }
