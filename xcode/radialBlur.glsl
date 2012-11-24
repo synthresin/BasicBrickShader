3069de2661ff62a3afac1b5339eea794e7ec953b
synthresin <synthresin@me.com>
2012-11-24 18:25:41 +0900

    화질적으로...

--- xcode/radialBlur.glsl
+++ xcode/radialBlur.glsl
@@ -17,14 +17,21 @@ void main() {
     
     vec3 color = vec3(0.0, 0.0, 0.0);
     // 오른쪽 위
-    if (p.x > 0.0 && p.y > 0.0) {
-        color = texture2D(tex0, gl_TexCoord[0].st).xyz;
-    } else if (p.x > 0.0 && p.y < 0.0) { // 오른쪽 아래
-        color = texture2D(tex0, gl_TexCoord[0].st + vec2(0.0, 0.5)).xyz;
-    } else if (p.x < 0.0 && p.y > 0.0) { // 왼쪽 위
-        color = texture2D(tex0, gl_TexCoord[0].st + vec2(0.5, 0.0)).xyz;
-    } else if (p.x < 0.0 && p.y < 0.0) { // 왼쪽 아래
-        color = texture2D(tex0, gl_TexCoord[0].st + vec2(0.5, 0.5)).xyz;
+    if (p.y > 0.0) {
+        vec2 newVec = gl_TexCoord[0].st + vec2(0.05 * -time, 0.0);
+        if (newVec.x < -1.0) {
+            color = vec3(0.0,0.0,0.0);
+        } else {
+            color = texture2D(tex0, gl_TexCoord[0].st + vec2(0.05 * -time, 0.0)).xyz;
+        }
+    } else {
+        vec2 newVec = gl_TexCoord[0].st + vec2(0.05 * time, 0.0);
+        if (newVec.x > 1.0) {
+            color = vec3(0.0,0.0,0.0);
+        } else {
+            color = texture2D(tex0, gl_TexCoord[0].st + vec2(0.05 * time, 0.0)).xyz;
+        }
+        
     }
 
     gl_FragColor = vec4(color, 1.0);
